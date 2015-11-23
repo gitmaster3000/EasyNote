f499cfb6e8541023632f285b49bdd207ed850fb7
theabster <abdullahkahn93@gmail.com>
2015-11-23 13:02:51 +0500

    heybey

--- app/src/main/java/afk/easynote/MainActivity.java
+++ app/src/main/java/afk/easynote/MainActivity.java
@@ -7,10 +7,13 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
     DrawerLayout mDrawerLayout;
@@ -18,11 +21,22 @@ public class MainActivity extends AppCompatActivity{
     FragmentManager mFragmentManager;
     FragmentTransaction mFragmentTransaction;
 
+    ListView listView;
+    List<String>  tags;
+    DatabaseHandler mHandler;
+    ArrayAdapter <String> mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHandler = new DatabaseHandler(this);
+        tags = mHandler.getAllTags();
+
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mAdapter = new ArrayAdapter<String>(this, R.layout.list, tags);
+
 
 
         mFragmentManager = getSupportFragmentManager();
@@ -32,15 +46,18 @@ public class MainActivity extends AppCompatActivity{
         android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
         ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout, toolbar,R.string.app_name,
                 R.string.app_name);
-        ListView listView = (ListView)findViewById(R.id.left_drawer);
-        listView.setAdapter(new ArrayAdapter<Integer>(this,R.layout.list, new Integer[]{1, 2, 3, 4}));
+        listView = (ListView)findViewById(R.id.left_drawer);
+        listView.setAdapter(mAdapter);
 
 
         mDrawerLayout.setDrawerListener(mDrawerToggle);
 
         mDrawerToggle.syncState();
 
+        //listView.getOnItemLongClickListener;
+
         final EditText mEditText = (EditText) findViewById(R.id.EditText01);
+        mEditText.setImeActionLabel("Add", KeyEvent.KEYCODE_ENTER);
         mEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
@@ -48,7 +65,10 @@ public class MainActivity extends AppCompatActivity{
                     String tag = mEditText.getText().toString();
                     if (!tag.matches("")) {
                         //this change here
-
+                        mHandler.addTag(tag);
+                        tags.add(tag);
+                        mAdapter.notifyDataSetChanged();
+                        mEditText.setText("");
                     }
                 }
             }
